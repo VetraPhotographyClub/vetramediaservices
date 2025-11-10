7487e1c81937b3ffe1077e35434725581cadba78
VetraPhotographyClub <vetraphotographyclub@icloud.com>
2025-11-10 15:00:29 -0800

    Add allowedHosts for vetramediaservices.com in Vite preview config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    preview: {
+  preview: {
     host: true,
     strictPort: true,
+    aallowedHosts: ['vetramediaservices.com','www.vetramediaservices.com'],
   },
 })
